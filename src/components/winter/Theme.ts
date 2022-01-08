3d998aa2f206c00b13e5c801be81de59e81f5c0e
seongjin-an <seongjin4us@gmail.com>
2022-01-08 23:06:34 +0900

    theme components

--- src/components/winter/Theme.ts
+++ src/components/winter/Theme.ts
@@ -8,12 +8,64 @@ type Key<T> = {
 }
 
 export interface Theme{
-    palette: Record<string, any> & IPalette;
+    // palette: Record<string, any>;
+    palette: {[K in keyof IPalette]: IPalette[K]};
+}
+export interface IPalette{
+    // [K: string]: object
+    common: ICommonColors;
+    primary: IColors;
+    secondary: IColors;
+    success: IColors;
+    error: IColors;
+    warning: IColors;
+    info: IColors;
+}
+export type PickedPalette = {
+    [K in keyof IPalette]?: IPalette[K]
+}
+export interface ICommonColors{
+    black: string;
+    white: string;
+}
+export type TCommonColors = {
+    [K in keyof ICommonColors]?: ICommonColors[K]
+}
+export interface IColors{
+    light: string;
+    main: string;
+    dark: string;
+}
+export type TPickedColors = {
+    [K in keyof IColors]?: IColors[K]
 }
 
-export interface IPalette {
-    primary: Key<IColor>
-}
+// const iKind: IPalette = {
+//     common: {
+//
+//     },
+//     primary: {
+//
+//     },
+//     secondary: {
+//
+//     },
+//     success: {
+//
+//     },
+//     error: {
+//
+//     },
+//     warning: {
+//
+//     },
+//     info: {
+//
+//     }
+// }
+// export interface IPalette {
+//     primary: Key<IColor>
+// }
 
 export type PartialTypeObject = { [P in keyof IPalette]?: Partial<IPalette[P]> };
 
@@ -21,5 +73,6 @@ const imsi:PartialTypeObject = {
     primary: {
         dark: "",
         light: ""
+
     }
 }
--- src/components/winter/WinterComponent.tsx
+++ src/components/winter/WinterComponent.tsx
@@ -2,6 +2,7 @@ import React, {useState, MouseEvent} from "react";
 import styled from "@emotion/styled";
 import {SxProps} from "./sx";
 import {Theme} from "./Theme";
+import {customTheme} from "../../data/Theme";
 
 interface IProps {
     sx: SxProps<Theme>
@@ -19,16 +20,17 @@ const WinterComponent:React.FC<IProps> = ({imsi,children, sx}) => {
     console.log(typeof sx);
     const [num, setNum] = useState<number>(0);
     // const [theme, setTheme] = useState<Theme>()
-    const theme: Theme = {palette: {primary: {light: "orange", dark: "red"} }};
-    if(typeof sx === 'function'){
-        return<Container sx={ sx(theme) }>
-            {children}
-        </Container>
-    }
+    // const theme: Theme = {palette: {primary: {light: "orange", dark: "red"} }};
+
     const handleClick = () => {
         console.log('winter')
         setNum(prev => prev+1)
         return imsi(num)
+    }
+    if(typeof sx === 'function') {
+        return <Container sx={sx(customTheme)}>
+            {children}
+        </Container>
     }
     return<Container sx={sx}>
         {children}
